56cee01416d05dd27e555afdc4853a89feecf337
Wilsomwong <wilsomwong040727@gmail.com>
2026-06-04 16:13:46 +0800

    Update: keep login status until sign out.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:ScamTap/pages/home.dart';
+import 'package:ScamTap/widgets/navibar.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:ScamTap/pages/login_page.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -39,7 +42,20 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
       ),
-      home: const LoginPage(),
+      home: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Scaffold(
+              body: Center(child: CircularProgressIndicator()),
+            );
+          }
+          if (snapshot.hasData) {
+            return const Navibar();
+          }
+          return const LoginPage();
+        },
+      ),
     );
   }
 }
